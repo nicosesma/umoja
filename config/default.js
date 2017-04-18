8d955b95b669b6c1f2f7dc428c1732a0a7ca187c
nicosesma <nicosm310@gmail.com>
2017-04-17 17:09:08 -0700

    WIP knex

--- config/default.js
+++ config/default.js
@@ -2,6 +2,6 @@ require('./environment')
 
 module.exports = {
   APP_ROOT: process.env.APP_ROOT,
-  SESSION_KEY: process.env.SESSION_KEY,
-  COOKIE_LIFETIME: 1000 * 60 * 60 * 24
+  COOKIE_LIFETIME: (1000 * 60 * 60 * 24),
+  SESSION_KEY: process.env.SESSION_KEY
 }
---
+++ knexfile.js
@@ -0,0 +1,24 @@
+const defaultConfig = env => {
+  const connectionString = (
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.USER}@localhost:5432/vendor-map-${env}`
+  )
+
+  return {
+    client: 'postgresql',
+    connection: connectionString,
+    migrations: {
+      directory: __dirname + 'db/migrations',
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: __dirname + `db/seeds/${env}`
+    }
+  }
+}
+
+module.exports = {
+  development: defaultConfig('development'),
+  production: defaultConfig('production'),
+  test: defaultConfig('test')
+}
